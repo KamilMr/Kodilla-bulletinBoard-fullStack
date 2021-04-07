055a6491ddb57c34d311e78fe8ad1d5b8c85f43a
Kamil Mrowka <kamil.mrowka@outlook.com.au>
2021-04-07 11:47:56 +0200

    add newDate func

--- src/components/views/PostAdd/PostAdd.js
+++ src/components/views/PostAdd/PostAdd.js
@@ -49,13 +49,26 @@ class Component extends React.Component {
     this.setState({ post: { ...post, [name]: value } });
   }
 
+  newDate = () => {
+    const dateObj = new Date();
+    const month = dateObj.getUTCMonth() + 1; //months from 1-12
+    const day = dateObj.getUTCDate();
+    const year = dateObj.getUTCFullYear();
+
+    let newdate = day + "/" + month + "/" + year;
+    return newdate
+  }
+
+
   submitForm = (e) => {
     e.preventDefault();
     const { post } = this.state;
     const { newPost } = this.props;
-    newPost(post);
-  };
-
+    this.setState({post: {...post, publication: this.newDate()}});
+    setTimeout(() => {
+      newPost(this.state.post);
+    }, 1000);
+  }
   render() {
     const { updateTextField, submitForm } = this;
     const { post } = this.state;
@@ -110,7 +123,7 @@ class Component extends React.Component {
             </Grid>
           </Grid>
           <Button className={styles.margin_top} startIcon={<SaveIcon />}
-            variant="contained" type="submit">Save</Button>
+            constiant="contained" type="submit">Save</Button>
         </form>
       </div>
     );
